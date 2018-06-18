662ce2fda009cc194278555445f88de37556813d
Radames J Halmeman <rjhalmeman@gmail.com>
2018-06-18 06:38:52 -0300

    projeto que controla servo motor

---
+++ src/QualAVersaoDoJava.java
@@ -0,0 +1,12 @@
+
+public class QualAVersaoDoJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String libPathProperty = System.getProperty("java.library.path");
+        System.out.println(libPathProperty);
+    }
+
+}
